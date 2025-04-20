a4e4e26510200f294bb1a28a432c7c40d33bf22a
Jan Hoon <janhoon97@gmail.com>
2025-04-20 17:38:19 +0100

    chore: split lib and main

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,5 @@
-use std::io;
+use cadac::print_my_name;
 
 fn main() {
-    let mut input = String::new();
-
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Failed to read line");
-
-    let input_int: u32 = input
-        .trim()
-        .parse()
-        .expect("Failed to parse input as integer");
-
-    println!("Input: {input_int}");
-
-    println!("Hello, world!");
+    print_my_name();
 }
